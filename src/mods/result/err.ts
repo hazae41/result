714295f86be6b4c0fb727511ed9d39d178a90317
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-04-30 22:42:28 +0200

    cast

--- src/mods/result/err.ts
+++ src/mods/result/err.ts
@@ -1,4 +1,5 @@
 import { None, Some } from "@hazae41/option"
+import { Class } from "libs/reflection/reflection.js"
 
 export type ErrInner<E> = E extends Err<infer T> ? T : never
 
@@ -12,8 +13,40 @@ export class Err<T = unknown>  {
     return new this(inner)
   }
 
+  /**
+   * 
+   * @param message 
+   * @param options 
+   * @returns 
+   */
   static error(message: string, options?: ErrorOptions) {
     return new this(new Error(message, options))
+  }
+
+  /**
+   * Try to cast `e` into `type` and return `Err(e)`, throw `e` if unable to do so
+   * @param e 
+   * @param type 
+   * @returns 
+   */
+  static cast<T>(e: unknown, type: Class<T>) {
+    if (e instanceof type)
+      return new this(e)
+    else
+      throw e
+  }
+
+  /**
+   * Try to cast `e` into `type` and return `Err(e)`, return `Err(or)` if unable to do so
+   * @param e 
+   * @param type 
+   * @returns 
+   */
+  static castOr<T>(e: unknown, type: Class<T>, or: T) {
+    if (e instanceof type)
+      return new this(e)
+    else
+      return new this(or)
   }
 
   isOk(): false {
