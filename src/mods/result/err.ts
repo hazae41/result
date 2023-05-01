4b12e1fe8f2c5975f59d1170094907dbdb807626
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-01 17:17:55 +0200

    clean

--- src/mods/result/err.ts
+++ src/mods/result/err.ts
@@ -1,7 +1,6 @@
 import { None, Some } from "@hazae41/option"
 import { Promiseable } from "libs/promises/promises.js"
 import { Class } from "libs/reflection/reflection.js"
-import { Result } from "./result.js"
 
 export type ErrInner<E> = E extends Err<infer T> ? T : never
 
