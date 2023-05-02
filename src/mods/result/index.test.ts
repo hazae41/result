98138e371c240a615b758774b54b748c7fd002b0
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-02 18:02:06 +0200

    clean

--- src/mods/result/index.test.ts
+++ src/mods/result/index.test.ts
@@ -1,12 +1,43 @@
+import { assert, test, throws } from "@hazae41/phobos";
+import { Err } from "./err.js";
 import { Ok } from "./ok.js";
 import { Result } from "./result.js";
 
-export * from "./err.test.js";
-
-function tryVoid() {
-  return Ok.void()
-}
-
 function doNotRun(result: Result<string, Error>) {
-  result.andThenSync(tryVoid).unwrap()
+  result.andThenSync(Ok.void).unwrap()
 }
+
+class FirstError extends Error {
+  name = "first"
+}
+
+class SecondError extends Error {
+  name = "second"
+}
+
+await test("try-catch", async ({ message }) => {
+
+  assert(throws(() => Result.unthrowSync(() => {
+    throw new Error()
+  }, Error)), `Should have not been catched`)
+
+  assert(!throws(() => Result.unthrowSync(() => {
+    new Err(new Error()).throw()
+
+    return Ok.void()
+  })), `Should have been catched`)
+
+  assert(!throws(() => Result.unthrowSync(() => {
+    new Err(new Error()).throw()
+
+    return Ok.void()
+  }, Error, TypeError)), `Should have been catched`)
+
+  assert(throws(() => Result.unthrowSync<void, FirstError | SecondError>(() => {
+    new Err(new Error()).throw()
+
+    return Ok.void()
+  }, FirstError, SecondError)), `Should have not been catched`)
+
+  console.log(message)
+})
