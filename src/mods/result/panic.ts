a12cda91e6dc2ddd573dcbb94df9f61f7fc73c3c
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-23 12:20:13 +0200

    panic

---
+++ src/mods/result/panic.ts
@@ -0,0 +1,20 @@
+export class Panic extends Error {
+  readonly #class = Panic
+  readonly name = this.#class.name
+}
+
+export namespace Panic {
+
+  /**
+   * Throw `error` if `Panic`, return `error` otherwise
+   * @param error 
+   * @returns error if not `Panic`
+   * @throws error if `Panic`
+   */
+  export function unthrow<T>(error: T): Exclude<T, Panic> {
+    if (error instanceof Panic)
+      throw error
+    return error as Exclude<T, Panic>
+  }
+
+}
