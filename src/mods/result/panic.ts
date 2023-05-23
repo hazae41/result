7eb4b6ce012dac5ac82a3e80dd549ed174d0d647
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-23 15:53:29 +0200

    clean panic

--- src/mods/result/panic.ts
+++ src/mods/result/panic.ts
@@ -7,30 +7,3 @@ export class Panic extends Error {
   }
 
 }
-
-export namespace Panic {
-
-  /**
-   * Throw `error` if `Panic`, return `error` otherwise
-   * @param error 
-   * @returns error if not `Panic`
-   * @throws error if `Panic`
-   */
-  export function rethrow<T>(error: T): Exclude<T, Panic> {
-    if (error instanceof Panic)
-      throw error
-    return error as Exclude<T, Panic>
-  }
-
-  /**
-   * Convert error to `Panic` if not already
-   * @param error 
-   * @returns `error` if `Panic`, `Panic.from(error)` otherwise
-   */
-  export function castOrFrom(error: unknown) {
-    if (error instanceof Panic)
-      return error
-    return Panic.from(error)
-  }
-
-}
