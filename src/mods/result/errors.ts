e69a9fa8ab135fe507c5c0b3726f36b6094e2820
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-23 11:56:42 +0200

    warnOrThrow

--- src/mods/result/errors.ts
+++ src/mods/result/errors.ts
@@ -1,3 +1,13 @@
 export class Panic extends Error {
   readonly #class = Panic
 }
+
+export namespace Panic {
+
+  export function warnOrThrow(e: unknown) {
+    if (e instanceof Panic)
+      throw e
+    console.warn(e)
+  }
+
+}
