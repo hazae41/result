a2044521a460e7b4b539b08990011a324e843eaa
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-23 15:14:56 +0200

    panic

--- src/mods/result/panic.ts
+++ src/mods/result/panic.ts
@@ -1,6 +1,11 @@
 export class Panic extends Error {
   readonly #class = Panic
   readonly name = this.#class.name
+
+  static from(cause: unknown) {
+    return new Panic(undefined, { cause })
+  }
+
 }
 
 export namespace Panic {
